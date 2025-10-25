b0cfc0d70d58ded3369c871c839552c1b2b65bbd
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-10-25 13:28:58 +0530

    CORS policy updated

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://proofchain.pages.dev/"],
+    allow_origins=["http://localhost:5173","https://proofchain.pages.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
